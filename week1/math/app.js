82742735f2cc18188f83d5ef16101127a264abc4
tekelia-powe <tekelia.powe@bryanuniversity.edu>
2020-11-17 21:25:20 -0600

    Week1 Assignments

---
+++ week1/math/app.js
@@ -0,0 +1,7 @@
+var math = require("./math.js")
+
+//const result = sum(10,20)
+console.log(math.sum(10,20))
+console.log(math.subtract(20,10))
+console.log(math.multiply(4,15))
+console.log(math.divide(20,10))
