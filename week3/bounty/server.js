9a0d00dcf979162d484bca9e087bdd6bec19660a
tekelia-powe <tekelia.powe@bryanuniversity.edu>
2020-12-12 08:26:39 -0600

    Week3 Assignments

---
+++ week3/bounty/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use("/bounty", require('./routes/bountyRouter.js'))
+
+
+app.listen(9000, () => {
+    console.log("The server is running on Port 9000");
+})
